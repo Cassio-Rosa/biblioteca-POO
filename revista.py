60325051c62586ac80db6fb81d05194edd8c39be
Cassio Rosa <cassioasrosa@gmail.com>
2026-03-02 16:49:01 -0300

    add revista

---
+++ revista.py
@@ -0,0 +1,16 @@
+from item_bilbioteca import Item_Biblioteca
+
+class Revista(Item_Biblioteca):
+    def __init__(self, id, titulo, ano, disponivel, edicao, mes):
+        super().__init__(id, titulo, ano, disponivel)
+        self.__edicao = edicao
+        self.__mes = mes
+
+    def exibir_detalhes(self):
+        print(F"                 REVISTA")
+        super().exibir_detalhes()
+        print(f"          Edição: {self.__edicao}")   
+        print(f"          Mês de Lançamento: {self.__mes}")
+
+cosmania = Revista(9, "WOWOWOOWW", 1999, True, 9, "Abril")
+cosmania.exibir_detalhes()
